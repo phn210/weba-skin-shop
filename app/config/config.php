9787861b6032b9ee268e2b2b329d40dd5b0abefc
phn210 <57532941+phn210@users.noreply.github.com>
2021-05-17 00:51:03 +0700

    PHP MVC core

---
+++ app/config/config.php
@@ -0,0 +1,16 @@
+<?php
+
+    // Database params
+    define('DB_HOST', 'localhost:3307');
+    define('DB_USER', 'root');
+    define('DB_PASS', '');
+    define('DB_NAME', 'web_db');
+
+    // APPROOT
+    define('APPROOT', dirname(dirname(__FILE__)));
+
+    // URLROOT (Dynamic links)
+    define('URLROOT', 'localhost/weba-skin-shop');
+
+    // Sitename
+    define('SITENAME', 'WebA Skin Shop');
---
+++ app/models/User.php
@@ -0,0 +1,8 @@
+<?php
+class User {
+    private $db;
+
+    public function __construct() {
+        $this->db = new Database;
+    }
+}
