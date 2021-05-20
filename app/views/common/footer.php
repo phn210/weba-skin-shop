939898063ee7673b47c67e5bb294d29a36782c9a
phn210 <57532941+phn210@users.noreply.github.com>
2021-05-20 15:30:29 +0700

    add header and footer

--- app/views/common/footer.php
+++ app/views/common/footer.php
@@ -1,4 +1,8 @@
-</body>
-<hr>
-FOOTER CONTENT
+        <footer>
+            <div class="company_name">Company Full Name</div>
+            <div class="contact">Company's Contact</div>
+            <div class="contact_navigator">Contact Navigator (FB, IG, Youtube...)</div>
+            <div class="map"></div>
+        </footer>
+    </body>
 </html>
--- app/views/common/header.php
+++ app/views/common/header.php
@@ -1,16 +1,121 @@
 <!DOCTYPE html>
-<html>
+<html lang="en-US" class="hide-scroll">
+<head> 
+    <title><?php echo SITENAME; ?></title>
 
-<head>
-  <meta charset="UTF-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <style>
+    <meta charset="utf-8" />
 
-  </style>
+    <meta name="viewport" content="initial-scale=1.0,maximum-scale=1.0,user-scalable=no,width=device-width">
+
+    <style>
+        body {
+            margin: 0;
+            padding: 0;
+        }
+
+        header {
+            position: fixed;
+            top:0;
+            background-color: #b3e6ff;
+            width: 100%;
+            height: auto;
+            display: flex;
+            align-items: center;
+            justify-content: space-around;
+        }
+
+        header > div {
+            align-items: center;;
+        }
+
+        header div :nth-child(odd){
+            
+        }
+
+        .logo {
+            flex: 1;
+        }
+
+        .navigator {
+            flex: 10;
+            display: flex;
+            justify-content: space-around;
+            align-items: center;
+        }
+
+        
+        .navigator h3 {
+            margin: 0;
+            text-align: center;
+        }
+
+        .navigator > div {
+            align-items: center;
+            flex-basis: 20%;
+            padding: 0 1rem 0 1rem;
+        }
+
+
+        .search {
+            flex: 3;
+        }
+
+        .cart {
+            flex: 1;
+        }
+
+    </style>
+
+    
 
 </head>
+    <body>
+        <header>
+            <div class="logo">
+                <img src="test.jpg" alt="logo" style="width: 5rem;">
+            </div>
 
-<body>
-    HEADER CONTENT
-    <hr>
+            <div class="navigator">
+                <div>
+                    <a href="">
+                        <h3>Home</h3>
+                    </a>
+                </div>
+
+                <div>
+                    <a href="">
+                        <h3>Product</h3>
+                    </a>
+                </div>
+
+                <div>
+                    <a href="">
+                        <h3>Design</h3>
+                    </a>
+                </div>
+
+                <div>
+                    <a href="">
+                        <h3>About Us</h3>
+                    </a>
+                </div>
+
+                <div>
+                    <a href="">
+                        <h3>Service</h3>
+                    </a>
+                </div>
+            </div>
+
+            <div class="search">
+                <input type="text" placeholder="Search..">
+                <img src="icon_search.jpg" alt="icon_search">
+            </div>
+
+            <div class="cart">
+                <a href="">
+                    <img src="icon_cart.jpg" alt="icon_cart" style="width:40px; height:auto">
+                </a>   
+            </div>
+            
+        </header>
