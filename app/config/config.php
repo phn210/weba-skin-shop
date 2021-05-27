3fa67556c991b79a68b38354a2bf8e26c82905ed
phn210 <57532941+phn210@users.noreply.github.com>
2021-05-27 13:50:21 +0700

    update config

--- app/config/config.php
+++ app/config/config.php
@@ -4,7 +4,7 @@
     define('DB_HOST', 'localhost:3307');
     define('DB_USER', 'root');
     define('DB_PASS', '');
-    define('DB_NAME', 'web_db1');
+    define('DB_NAME', 'web_db');
 
     // APPROOT
     define('APPROOT', dirname(dirname(__FILE__)));
@@ -17,11 +17,3 @@
 
     // Sitename
     define('SITENAME', 'WebA Skin Shop');
-
-    /*
-    - Front-end(html/css/js): Hằng + Nguyên
-    - Back-end: Long + Toàn + Nguyên
-        + Home & Navigator: Nguyên
-        + Tìm kiếm + Danh sách sản phẩm: Long
-        + Chi tiết sản phẩm + Quản lý giỏ hàng: Toàn
-    */
